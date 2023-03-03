9ac213721ed548827ecf4e05a9efe10b5e136807
Keshav Kumar <keshavkumar8122@gmail.com>
2023-03-04 01:10:54 +0530

    patterns

---
+++ 1.2 Patterns/1.cpp
@@ -0,0 +1,27 @@
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+    void printSquare(int n) {
+        for(int i=1;i<=n;i++){
+            cout<<"*"<<" ";
+            for(int j=1;j<n;j++){
+                cout<<"*"<<" ";
+            }
+            cout<<endl;
+        }
+    }
+
+int main()
+{
+    int n;
+    cin>>>n;
+    printSquare(n);
+    return 0;
+}
